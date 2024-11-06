d19868d9661b1a0f4339ae6c5dfa9e91c8267db1
Valentin <zhe-valentin@mail.ru>
2024-11-07 05:03:57 +0700

    ДЗ 2.7 Коллекции. Многообразие реализаций.

--- src/main/java/org/skypro25/Collections/exception/EmployeeAlreadyAddedException.java
+++ src/main/java/org/skypro25/Collections/exception/EmployeeAlreadyAddedException.java
@@ -1,5 +1,9 @@
 package org.skypro25.Collections.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException() {
     }
